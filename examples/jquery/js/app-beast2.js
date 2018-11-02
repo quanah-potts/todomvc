ca64de77d8b07aad4712b2b3e78c6992b57c333b
quanah-potts <39603394+quanah-potts@users.noreply.github.com>
2018-11-02 19:26:52 -0400

    Update and rename app.js to app-beast2.js
    My solution to Beast 2 for Watch & Code. 
    
    This answer is a bit braindead, just replacing the call to util.store in render with like, six calls in all the other functions where the logical todos array is modified (so, toggle, toggleAll, destroy, destroyCompleted, update, and create). 
    
    However, as best I can tell, it works, and is not hard to understand, so I think it's OK. 
    
    Will update or modify if this solution proves unsatisfactory.

--- examples/jquery/js/app-beast2.js
+++ examples/jquery/js/app-beast2.js
@@ -70,7 +70,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -90,7 +89,8 @@ jQuery(function ($) {
 			this.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
-
+			
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +117,8 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -147,12 +149,15 @@ jQuery(function ($) {
 			});
 
 			$input.val('');
-
+			
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		edit: function (e) {
@@ -184,10 +189,13 @@ jQuery(function ($) {
 				this.todos[this.indexFromEl(el)].title = val;
 			}
 
+			util.store('todos-jquery', this.todos);
 			this.render();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			
+			util.store('todos-jquery', this.todos);
 			this.render();
 		}
 	};
